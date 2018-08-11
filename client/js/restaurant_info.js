161b2cf4c0e345da34f1443c9795829dd47e9963
Francisco Gutierrez <francisco.j.gtz@gmail.com>
2018-08-10 19:03:21 -0700

    Feat: Move the code that registers the sw to an independent file called sw_registration.js

--- client/js/restaurant_info.js
+++ client/js/restaurant_info.js
@@ -369,80 +369,13 @@ document.getElementById('button-favorite').addEventListener('click', function ()
     self.restaurant = responseRestaurant;
     var favButton = document.getElementById('button-favorite');
     if (responseRestaurant.is_favorite === 'true' || responseRestaurant.is_favorite === true) {
-      favButton.innerHTML = '★';
+      favButton.innerHTML = '\u2605';
       favButton.classList.add('button-favorite--favorite');
       favButton.classList.remove('button-favorite--not-favorite');
     } else {
-      favButton.innerHTML = '☆';
+      favButton.innerHTML = '\u2606';
       favButton.classList.add('button-favorite--not-favorite');
       favButton.classList.remove('button-favorite--favorite');
     }
   });
 });
-
-function notifySWUpdates(reg) {
-  console.log('There is a new Service Worker available');
-  // create button
-  var buttonSW = document.createElement('button');
-  buttonSW.classList.add('sw-button');
-  buttonSW.innerHTML = 'Update Available';
-  // append button
-  var docBody = document.getElementsByTagName('body')[0];
-  docBody.appendChild(buttonSW);
-  // onclick, post message
-  buttonSW.addEventListener('click', function () {
-    reg.postMessage({ activate: 'true' });
-  });
-}
-
-function trackSWStates(reg) {
-  var _this = this;
-
-  reg.addEventListener('statechange', function () {
-    if (_this.state == 'installed') {
-      notifySWUpdates(reg);
-    }
-  });
-}
-
-/**
- * This function registers the service worker
-*/
-function registerServiceWorker() {
-  navigator.serviceWorker.register('sw.js').then(function (reg) {
-    // refers to the SW that controls this page
-    if (!navigator.serviceWorker.controller) {
-      // page didn't load using a SW
-      // loaded from the network
-      return;
-    }
-
-    if (reg.waiting) {
-      // there's an update ready!
-      notifySWUpdates(reg.waiting);
-    }
-
-    if (reg.installing) {
-      // there's an update in progress
-      trackSWStates(reg.installing);
-    }
-
-    reg.addEventListener('updatefound', function () {
-      trackSWStates(reg.installing);
-    });
-
-    var reloading = void 0;
-    navigator.serviceWorker.addEventListener('controllerchange', function () {
-      if (reloading) return;
-      window.location.reload();
-      reloading = true;
-    });
-  }).catch(function (err) {
-    console.log('SW failed: ', err);
-  });
-}
-
-/**
- * Add service worker.
- */
-registerServiceWorker();
